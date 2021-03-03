ee78be9350639e02032b16e53004f2f7bc6113de
tukY Chyvekshyno <onar2000@gmail.com>
2021-03-03 22:44:49 +0200

    add JavaDoc to CellMap class

--- src/com/tuky/diploma/structures/CellMap.java
+++ src/com/tuky/diploma/structures/CellMap.java
@@ -1,4 +1,22 @@
 package com.tuky.diploma.structures;
 
+/**
+ * Create cell map from JSON file.
+ * JSON-file describes building structure
+ * Each object describes one floor (can't be repeated, may be as key)
+ * Object has attributes:
+ *      1.  floor
+ *      2.  name    [optional]
+ *      3.  door    (as the start point of room as wall array)
+ *          wall s also an object:
+ *              1.  length  (in cm/inch)
+ *              2.  door    (distance from start of wall in cm/inch, width)    [optional]
+ *                      is a new  room so is an array
+ *                      if door is an empty array, it's the exit of the floor
+ *              3.  vector  (of rotation from previous)     {l, r}
+ *              4.  degree  [90 grad as default]
+ *
+ * Start build floor from input door
+ */
 public class CellMap {
 }
